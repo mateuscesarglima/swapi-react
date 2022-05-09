f75a528a9a706a99108cf3676d7b9af6087235ca
Mateus Cesar <mateuscesarglima@gmail.com>
2022-05-09 15:22:36 -0300

    feat(STAR-5) footer implementation

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../layouts/header/Header";
 import Main from "../layouts/main/Main";
+import Footer from "../layouts/footer/Footer";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 };
