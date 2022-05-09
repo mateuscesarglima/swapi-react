b84ea1643a017945665db68a661a075297f0eec8
Mateus Cesar <mateuscesarglima@gmail.com>
2022-05-09 11:32:40 -0300

    feat(STAR-4) full main implementation

--- src/components/layouts/HomePageCard/HomeCard.jsx
+++ src/components/layouts/HomePageCard/HomeCard.jsx
@@ -1,12 +1,15 @@
 import "./HomeCard.css";
+import LinkButton from "../linkButton/LinkButton";
 
-const HomeCard = ({ title, number, description, img, alt }) => {
+const HomeCard = ({title, number, description, img, alt, content,to }) => {
   return (
     <>
       <div className="card-text-box">
         <p className="card-number">{number}</p>
         <h3 className="card-title">{title}</h3>
         <p className="card-description">{description}</p>
+        <p className="know-more">Would you like know more about {title}?</p>
+        <LinkButton to={to} customClass="small-font">{content}</LinkButton>
       </div>
       <figure className="step-img-box">
         <img src={img} alt={alt} className="step-img" />
--- src/components/layouts/main/Main.jsx
+++ src/components/layouts/main/Main.jsx
@@ -27,30 +27,45 @@ const Main = () => {
               Click on the buttons below to learn more
             </h2>
             <div className="card-container grid grid--2-cols grid--center-v">
-              <HomeCard title="Planets" number="01" img={planetTatooine} description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fugiat ea dignissimos tenetur, nihil animi? Assumenda delectus repudiandae nam veritatis." />
+              <HomeCard
+                title="Planets"
+                number="01"
+                img={planetTatooine}
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fugiat ea dignissimos tenetur, nihil animi? Assumenda delectus repudiandae nam veritatis."
+                content="Click here!"
+                to="/planets"
+              />
               <HomeCard
                 title="People"
                 number="02"
                 img="https://lumiere-a.akamaihd.net/v1/images/yoda-s-lightsaber-main_9ebf17d9.jpeg?region=372%2C0%2C536%2C536&width=320"
                 description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fugiat ea dignissimos tenetur, nihil animi? Assumenda delectus repudiandae nam veritatis."
+                content="Click here!"
+                to="/people"
               />
               <HomeCard
                 title="Starships"
                 number="03"
                 img="https://lumiere-a.akamaihd.net/v1/images/databank_alderaancruiser_01_169_c60ce268.jpeg?region=255%2C0%2C877%2C878&width=320"
                 description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fugiat ea dignissimos tenetur, nihil animi? Assumenda delectus repudiandae nam veritatis."
+                content="Click here!"
+                to="/starships"
               />
               <HomeCard
                 title="Species"
                 number="04"
                 img="https://lumiere-a.akamaihd.net/v1/images/tlj-abednedo-databank-main-image_bd8babfb.jpeg?region=341%2C0%2C878%2C878&width=320"
                 description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fugiat ea dignissimos tenetur, nihil animi? Assumenda delectus repudiandae nam veritatis."
+                content="Click here!"
+                to="/species"
               />
               <HomeCard
                 title="Vehicles"
                 number="05"
                 img="https://lumiere-a.akamaihd.net/v1/images/at-act-walker_78783f1b.jpeg?region=309%2C0%2C541%2C542&width=320"
                 description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fugiat ea dignissimos tenetur, nihil animi? Assumenda delectus repudiandae nam veritatis."
+                content="Click here!"
+                to="/vehicles"
               />
             </div>
           </section>
